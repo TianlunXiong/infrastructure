4d8b370147b1cb0c5ab04ed2b479ce3f1ab12577
tainlx <418219627@qq.com>
2021-03-14 18:38:00 +0800

    init

---
+++ docker/install.sh
@@ -0,0 +1,4 @@
+dnf install -y \
+yum-utils device-mapper-persistent-data lvm2
+https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.4.4-3.1.el7.x86_64.rpm
+docker-ce docker-ce-cli
