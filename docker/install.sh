81687e8ca18ad4221911a9147940f9ab4a8d83c7
tainlx <418219627@qq.com>
2021-03-15 00:54:36 +0800

    feat: docker 安装脚本::开启docker服务

--- docker/install.sh
+++ docker/install.sh
@@ -1,4 +1,9 @@
+yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+
 dnf install -y \
 yum-utils device-mapper-persistent-data lvm2
 https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.4.4-3.1.el7.x86_64.rpm
 docker-ce docker-ce-cli
+
+systemctl enable docker.service
+systemctl start docker.service
